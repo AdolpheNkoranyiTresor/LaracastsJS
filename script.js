312efdfebcedf3f9655f3879c7c962e0d5fdde72
AdolpheNkoranyiTresor <85619805+AdolpheNkoranyiTresor@users.noreply.github.com>
2023-09-19 13:06:25 +0300

    JavaScript types: how to work with strings

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-console.log('External script');
+const firstName = 'Adolphe';
+const lastName = "Nkoranyi";
+const jobTitle = `Web Developer`;
 
-document.querySelector('p').style.color = 'green';
+const storeName = "Adolphe's \"Special\" Cookies \\ Treats";
+
+const storeName2 = `Adolphe's "Special" Cookies \\ Treats`;
+
+const fullName = firstName + ' ' + lastName;
+
+const about = 'Name: ' + firstName + ' ' + lastName + ', Job: ' + jobTitle;
+
+const html = '\n'
+    + '<div>\n'
+    + 'Hi there\n'
+    + '</div>\n';
+
+
+//Rewrite the above codes using backticks or template strings or template literals (``)
+
+const fullName2 = `${firstName} ${lastName}`; //Looks much cleaner than the first const fullName
+const about2 = `Name: ${fullName}, Job: ${jobTitle}`; //Much cleaner than the first about varible
+
+const html2 = `
+<div>
+    Hi there ${fullName2}
+</div>
+`;
+
+console.log(html2);
+console.log(firstName.length); //to count number of charactors
+console.log(firstName[0]); //to grab a certain charactor using the array syntax
+console.log(firstName.charAt(1)); //to grab a certain charactor using the charAt Method
+console.log(firstName.toUpperCase()); //to concert all the charactors in the string to Uppercase
+console.log(firstName.toLowerCase()); //to concert all the charactors in the string to Lowercase
+console.log(firstName.includes('lphe')); //Returns true if searchString appears as a substring of the result of converting this object to a String, at one or more positions that are greater than or equal to position; otherwise, returns false.
+console.log(firstName.includes('Nko')); //Returns true if searchString appears as a substring of the result of converting this object to a String, at one or more positions that are greater than or equal to position; otherwise, returns false.
