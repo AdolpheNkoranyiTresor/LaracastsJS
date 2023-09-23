22450dc0fd64be3079d278dff5f9873cd26ee3eb
AdolpheNkoranyiTresor <85619805+AdolpheNkoranyiTresor@users.noreply.github.com>
2023-09-23 15:21:23 +0300

    If, else if statement & Ternary, short of  if-else

---
+++ newFile.js
@@ -0,0 +1,5 @@
+let = canIDrinkBeer =
+  age >= drinkingAge
+    ? (canIDrinkBeer = "I can drink Beer")
+    : (canIDrinkBeer = "I cannot take beer yet.");
+console.log(canIDrinkBeer);
--- script.js
+++ script.js
@@ -1,35 +1,81 @@
-//Function Expression
-const hello = function (firstName) {
-  return `hello ${firstName}`;
-};
+const age = 12;
+const drinkingAge = 18;
 
-//Arrow functions
-// const hello = (firstName) =>{
-//     return `hello ${firstName}`;
-// }
-
-// const hello = firstName => `hello ${firstName}`; //This is an implicit return
-
-console.log(hello('Adolphe'));
-
-
-const person = {
-    firstName: 'Adolphe',
-    lastName: 'Tresor',
-    age: 29,
-    howOldAmI: function () {
-        console.log(`I am ${this.age} years old`);
-    }
-};
-
-person.howOldAmI();
-
-
-// Callback function
-const button = document.querySelector('button');
-
-const greetings = function () {
-    console.log("Hello there!!!");
+if (age >= drinkingAge) {
+  console.log("I can drink beer!!");
+} else {
+  console.log("I cannot drink beer at my age!!");
 }
 
-button.addEventListener('click', greetings);
+const passwordTypedIn = "putyoursavedpassword123";
+const actualPassword = "password123";
+
+if (passwordTypedIn === actualPassword) {
+    console.log('Correct password, login in progress...');
+} else {
+    console.log("Incorrect password, please try again later");
+}
+
+
+const month = 'October';
+const day = 31;
+
+if (month === 'October' && day === 31) {
+  console.log(`It's Halloween!`);
+} else {
+  console.log("It's not Halloween");
+}
+
+//If, else if statment
+
+// > 80 = A
+// 70 - 80 = B
+// 60 - 70 = C
+// 50 - 60 = D
+// < 50 = F
+
+
+const grade = 82;
+
+if (grade >= 80) {
+  console.log("Latter grade: A");
+} else if (grade >= 70) {
+  console.log("Latter grade: B");
+} else if (grade >= 60) {
+  console.log("Latter grade: C");
+} else if (grade >= 50) {
+  console.log("Latter grade: D");
+} else {
+  console.log("Latter grade: F");
+}
+
+
+// Ternary
+
+// if (age >= drinkingAge) {
+//   console.log("I can drink beer!!");
+// } else {
+//   console.log("I cannot drink beer at my age!!");
+// }
+// This is a Ternary, a short form of if else statment
+age >= drinkingAge
+  ? console.log("I can drink beer!!")
+  : console.log("I cannot drink beer at my age!!");
+
+// let canIDrinkBeer;
+
+// if (age >= drinkingAge) {
+//     canIDrinkBeer = "I can drink beer!!";
+// } else {
+//     canIDrinkBeer = "I cannot drink beer at my age!!";
+// }
+
+// let = canIDrinkBeer = age >= drinkingAge ? 'I can drink Beer' : 'I cannot take beer yet.';
+
+// console.log(canIDrinkBeer);
+
+console.log(
+    age >= drinkingAge
+    ? "I can drink Beer"
+    : "I cannot take beer yet."
+);
