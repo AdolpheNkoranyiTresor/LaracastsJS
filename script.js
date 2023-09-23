5c52f1a32f6be666d693575c40ff8149e36d66b5
AdolpheNkoranyiTresor <85619805+AdolpheNkoranyiTresor@users.noreply.github.com>
2023-09-23 23:22:42 +0300

    Loops and Iterations

--- script.js
+++ script.js
@@ -1,51 +1,69 @@
-const trafficLight = "yellow";
+// for (let i = 0; i < 10; i++) {
+//   console.log(`i is ${i}`); //Prints out 0 to 9
+// }
 
-switch (trafficLight) {
-  case "Yellow":
-    console.log("Slow Down");
-    break;
-  case "Green":
-    console.log("GO");
-    break;
-  case "Red":
-    console.log("STOP");
-    break;
-  default:
-    console.log("Traffic Light not working");
+// const people = ["Adolphe", "Six", "Bobby"];
+
+// for (let i = 0; i < people.length; i++) {
+//   console.log(people[i]); //Prints out Adolphe, Six and Bobby
+// }
+
+//Break and Continue
+// for (let i = 0; i < 10; i++) {
+//   if (i === 4) {
+//     // break; // prints out 0 to 3
+//     continue; //prints out 0 to 9 but skips i === 4
+//   }
+//   console.log(`i is ${i}`);
+// }
+
+//While loops
+
+// let j = 0;
+// while (j < 10) {
+//   console.log(`j is ${j}`);
+//   j += 2;
+// }
+
+//DO while loop
+
+// let k = 0;
+// do {
+//   console.log(`k is ${k}`);
+//   k++;
+// } while (k < 10);
+
+//For in loop
+
+const person = {
+  firsName: 'Adolphe',
+  lastName: 'Nkoranyi',
+  job: 'Web Master',
+};
+
+for (const x in person) {
+  // console.log(x); //prints firstName, lastName and job
+  console.log(person[x]); //results the actual values: Adolphe, Nkoranyi and Web Master
 }
 
-const grade = 60;
+// for of loops
 
-switch (true) {
-  case grade >= 80:
-    console.log("Latter Grade: A");
-    break;
-  case grade >= 70:
-    console.log("Latter Grade: B");
-    break;
-  case grade >= 60:
-    console.log("Latter Grade: C");
-    break;
-  case grade >= 50:
-    console.log("Latter Grade: D");
-    break;
-  default:
-    console.log("Latter Grade: F");
-}
+const people = ["Adolphe", "Six", "Bobby"];
 
-const animal = "Dog";
+// for (const y of people) {
+//   console.log(y);
+// }
 
-switch (animal) {
-  case "Cow":
-  case "Giraffe":
-  case "Dog":
-  case "Pig":
-    console.log("This animal is NOT extinct");
-    break;
-  case "Dinosaur":
-  case "Dodo":
-    console.log("This animal is extinct");
-    break;
-  default:
-    console.log("I am not an animal of this kind.");
-}
+//foreach
+
+// people.forEach((person) => {
+//     console.log(person);
+// });
+
+//map
+
+const peoppleMap = people.map((person, index) => {
+  return `${index}:${person}`;
+});
+
+console.log(peoppleMap);
